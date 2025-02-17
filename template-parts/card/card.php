d0c50a9c23320e4d05809b318fae74b3a7fdc485
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-17 15:33:26 +0100

    standardizing style

--- template-parts/card/card.php
+++ template-parts/card/card.php
@@ -2,15 +2,24 @@
   $date = get_the_date('M j');
 ?>
 
+<!-- Note: For both SEO and accessibility, it’s generally better to have the heading inside the link -->
+<!-- <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> -->
+
 <div class="card">
+  
   <div class="card__image">
-    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+    <a href="<?php the_permalink(); ?>">
+      <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+    </a>
   </div>
 
   <div class="card__content">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <p class="date">Published <?php echo $date; ?></p>
-    <p><?php the_excerpt(); ?></p>
+    
+    <a href="<?php the_permalink(); ?>">
+      <h3><?php the_title(); ?></h3>
+    </a>
+    <span class="date">Published <?php echo $date; ?></span>
+    <p class="excerpt"><?php the_excerpt(); ?></p>
   </div>
 
 </div>
