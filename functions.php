2c822dc16e536e1fb9060cf4f9f82d243ee123ae
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-18 17:00:53 +0100

    adding back style

--- functions.php
+++ functions.php
@@ -1,16 +1,10 @@
 <?php 
 
-// Add CSS stylesheets to the theme
+// Add the main stylesheet to the theme
 function enqueue_theme_styles() {  
   wp_enqueue_style( 'style-index', get_template_directory_uri() . '/build/style-index.css', array(), wp_get_theme()->get('Version'));
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles');
-
-// Add Javascript files to the theme
-function enqueue_theme_scripts() {
-  wp_enqueue_script( 'main-js', get_template_directory_uri() . '/build/index.js', array(), wp_get_theme()->get('Version') );
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_theme_scripts');
 
 // Add post thumbail support to the theme
 add_theme_support('post-thumbnails');
