e80df71562b3764545290b6cdb2eb2e520556aa5
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-19 08:30:27 +0100

    finished darkmode

--- footer.php
+++ footer.php
@@ -37,7 +37,7 @@
     
     <div class="footer-bottom">
       <div><?php get_template_part( 'template-parts/components/theme-mode' ); ?></div>
-      <div class="copyrigtht">Copyright © 2025 AI Luxury Boutique</div>
+      <div class="copyright">Copyright © 2025 AI Luxury Boutique</div>
     </div><!-- footer-bottom --> 
 
   </div><!-- .container --> 
