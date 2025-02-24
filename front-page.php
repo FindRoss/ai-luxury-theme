434ff39a76c9e8601a3c8a41abb782469ebf4cba
sadok159 <gablesadok@gmail.com>
2025-02-24 19:27:39 +0200

    home

--- front-page.php
+++ front-page.php
@@ -10,22 +10,115 @@
   ?>
 
   <div class="container">
-
-    <h2>Latest Articles</h2>
-    
+<div class="camera-section">
+  <div class="camera-posts">
+  <h3>Latest Articles</h3>
     <?php
       if ($query->have_posts()) : 
+
+
         while ($query->have_posts()) : $query->the_post(); ?>
           
-          <h3><?php the_title(); ?></h3>
-          <p><?php the_excerpt(); ?></p>
+          <h4><?php the_title(); ?></h4>
+          
           <a href="<?php the_permalink(); ?>">Read full article</a>
 
         <?php 
         endwhile;
       endif; 
     ?>
+    </div>
+
+<div class="cameras">
+   
+<img src="https://s3-alpha-sig.figma.com/img/a035/5f2a/603a73465e7cb73eb9d70a7e38b31fd4?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=PLrDKxynUaUv6QBKuqaUcbpUGDmI8JhkUC3PsNY1lez3Nor9usR1kHAbJVPVoTzn59fIplJ8X7aVqWUFfPQb2-~qBoDU-Y7RFDVh9UkTZz7KeI2aU8M2K0pHrek0UqDABuLooCUkY02d-5cHaQQGtfObqpp0emQlYM-yBfbcFP3UVTwkpTgLmZLo5YWE7NPsJyT9xniJqvDrkv1~twmXZ~7X1DIHXO1G-V7nN1OTrHRDMltQSVnJYPBWSqyd5aImdPeU-iQemf0xuL17Eoa~iTjotoHM83gzDMiSNSOblEeNuvc2H~0LqKob-ySwmKgSrClEgkJ1YIkx~EI-DxAUeg__" alt="AI gadgets">
+<h2> The Best AI Security Cameras</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus debitis saepe necessitatibus illo provident. Aliquid quae corrupti natus magnam qui doloremque</p>
+    </div>
+
+
+<h3 class="deals">Deals</h3>
+
+    </div>
+<!-- CAMERA SECTION -->
+
+
+<div class="home-section">
+<h1> Home & Garden </h1>
+
+
+
+<?php 
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 3
+    ); 
+
+    $query = new WP_Query($args); 
+  ?>
+
+
+<div class="home-grid">
+<?php 
+    $args = array(
+      'post_type'      => 'post', 
+      'posts_per_page' => 4,
+      
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+
+      echo '<div class="card-grid">';
+        while ($query->have_posts()) : $query->the_post(); 
+          get_template_part( 'template-parts/card/card' );
+        endwhile; 
+      echo '</div>';
+    endif; ?>
+</div>
+<!-- HOME GRID -->
+</div>
+<!-- HOME & GARDEN SECTION -->
+
+
+
+
+
+
+<div class="security">
+
+<h1> Security </h1>
+
+
+<?php 
+    $args = array(
+      'post_type'      => 'post', 
+      'posts_per_page' => 3,
+      
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+
+      echo '<div class="card-grid">';
+        while ($query->have_posts()) : $query->the_post(); 
+          get_template_part( 'template-parts/card/card' );
+        endwhile; 
+      echo '</div>';
+    endif; ?>
+
+
+
+
+
+  
+  </div>
 
   </div>
+<!-- CONTAINER -->
+
+
 
 <?php get_footer(); ?>
