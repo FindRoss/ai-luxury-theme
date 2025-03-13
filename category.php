d6993fab4f44e34aaa3e0ba2e1351ac0fe2e518c
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-03-13 13:32:19 +0100

    variables complete

--- category.php
+++ category.php
@@ -15,50 +15,42 @@
   $category_description = $category->description;
 ?>
 
-<div class="category-heading">
-  <h1><?php echo $category_name; ?></h1>
-  <p><?php echo $category_description; ?></p>
-</div>
-  
-
-
+  <div class="category-header">
+    <h1 class="category-header__heading"><?php echo $category_name; ?></h1>
+    <p class="category-header__text"><?php echo $category_description; ?></p>
+  </div>
 
 <?php 
+  $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
 
-$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+  $args = array(
+    'post_type'      => 'post', 
+    'posts_per_page' => 3,
+    'paged'          => $paged,
+    'cat'            => $category_id,
+  );
 
-    $args = array(
-      'post_type'      => 'post', 
-      'posts_per_page' => 3,
-      'paged'          => $paged,
-      'cat'            => $category_id,
-    );
+  $query = new WP_Query($args);
+  if ($query->have_posts()) : 
+    echo '<div class="card-grid">'; 
+    while ($query->have_posts()) : $query->the_post(); 
+      get_template_part( 'template-parts/card/osaka-card' );
+    endwhile; 
+    echo '</div>';
+  endif; ?>
 
-    $query = new WP_Query($args);
-    if ($query->have_posts()) : 
-      echo '<div class="card-grid">'; 
-      while ($query->have_posts()) : $query->the_post(); 
-        get_template_part( 'template-parts/card/osaka-card' );
-      endwhile; 
-      echo '</div>';
-    endif; ?>
+  <div class="pagination">
+    <?php
+    $big = 999999999; // need an unlikely integer
 
-<div class="pagination">
-<?php
-// Pagination
-$big = 999999999; // need an unlikely integer
+    echo paginate_links( array(
+      'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+      'format'  => '?paged=%#%',
+      'current' => max( 1, get_query_var('paged') ),
+      'total'   => $query->max_num_pages
+    ));
 
-
-echo paginate_links( array(
-  'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-  'format'  => '?paged=%#%',
-  'current' => max( 1, get_query_var('paged') ),
-  'total'   => $query->max_num_pages
-));
-
-// Reset post data
-wp_reset_postdata();
-?>
-</div>
+    wp_reset_postdata(); ?>
+  </div>
 </div>
 <?php get_footer(); ?>
