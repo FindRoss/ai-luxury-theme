9760c2a38503fd3a618c5124a01dec6f1d5a291d
Ross Findlay <rosspfindlay@gmail.com>
2025-05-14 08:18:25 +0200

    post header

--- single.php
+++ single.php
@@ -2,6 +2,9 @@
 
 <?php // get_template_part( 'template-parts/banner/background-image' ); ?>
 
+
+<?php // get_template_part( 'template-parts/search/search-bar' ); ?> 
+
 <?php get_template_part( 'template-parts/breadcrumbs/breadcrumbs' ); ?>
 
 <div class="container"> 
@@ -9,17 +12,17 @@
   <article>
 
     <!-- Heading -->
-    <header class="heading">
+    <section class="heading full-width">
       <div class="heading__content">
         <h1><?php the_title(); ?></h1>
         <div class="date">Updated Jan 25</div>
       </div>
-      <div class="heading__image full-width">
+      <div class="heading__image ">
         <?php if (has_post_thumbnail()): ?>
           <img src="<?php echo get_the_post_thumbnail_url(null, 'large'); ?>" />
         <?php endif; ?>
       </div>
-    </header>
+    </section>
 
     <!-- Section -->
     <section class="content">  
@@ -36,13 +39,14 @@
     $args = array(
       'post_type'      => 'post', 
       'posts_per_page' => 3,
+      
     );
+
     $query = new WP_Query($args);
 
-    echo '<div class="section-heading centered"><h2>Read More</h2></div>'; 
-
     if ($query->have_posts()) :
-      echo '<div class="konto-section">';
+
+      echo '<div class="card-grid">';
         while ($query->have_posts()) : $query->the_post(); 
           get_template_part( 'template-parts/card/osaka-card' );
         endwhile; 
