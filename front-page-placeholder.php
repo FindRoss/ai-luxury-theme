cf5cc4d2af2c6b578416e96df3c0db84369db065
Ross Findlay <rosspfindlay@gmail.com>
2025-05-26 13:37:24 +0200

    just one little thing

---
+++ front-page-placeholder.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+
+<div class="container">
+  <div class="frontpage-placeholder">
+    <h1>AI Luxury Boutique is coming soon</h1>
+  </div>
+</div>
+  
+<?php get_footer(); ?> 
